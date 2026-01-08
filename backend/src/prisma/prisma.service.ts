f6135d0a05d24b25ca3d7f4f9bb19a657bb9f45f
daydream-skybreak <samueldesta323@gmail.com>
2026-01-08 13:10:22 +0300

    feat: adds cors

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -9,11 +9,8 @@ const environment = env.config();
 export class PrismaService extends PrismaClient {
   constructor() {
     const connectionString = environment.parsed?.DATABASE_URL;
-    console.log(`Connected as ${connectionString}`);
     const pool = new pg.Pool({ connectionString });
     const adapter = new PrismaPg(pool);
-    console.log('Database URL:', process.env.DATABASE_URL);
-    console.log('\n\n\n\n\n\nabove is the database url\n\n\n\n\n\n');
     super({ adapter });
   }
 
